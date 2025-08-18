2be5e107a3c2754054494c5b2bf8699932ab44e6
d3xb0t <d3xb0t@gmail.com>
2025-08-18 18:53:35 -0500

    build(env): update environment configuration
    - Update .env file format from key:value to key=value
    - Change PORT value from 3000 to 4001
    - Remove default PORT value in env.js, now fully relying on .env file

--- src/config/env.js
+++ src/config/env.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT
 export const NODE_ENV = process.env.NODE_ENV || 'development'
